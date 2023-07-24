72bb643c30da118d87d2b4a5ebc75fbec58a9aec
KazutoKiritoKirigaya <KazutoKiritoKirigaya@users.noreply.github.com>
2023-07-24 12:20:50 +0100

    Added console board

--- src/noughts_and_crosses.py
+++ src/noughts_and_crosses.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import numpy as np
 
 from constants import *
 
@@ -9,8 +10,20 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Noughts and Crosses AI")
 screen.fill(BG_COLOUR)
 
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((ROWS, COLS)) # => [[0. 0. 0.] [0. 0. 0.] [0. 0. 0.]]
+
+    def mark_sqr(self, row, col, player):
+        self.squares[row][col] = player
+
+    def empty_square(self, row, col):
+        return self.squares[row][col] == 0
+
 class Game:
     def __init__(self):
+        self.board = Board()
+        self.player = 1
         self.show_lines()
 
     def show_lines(self):
@@ -21,16 +34,31 @@ class Game:
         # Horizontal
         pygame.draw.line(screen, LINE_COLOUR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOUR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
+    
+    def next_turn(self):
+        self.player = self.player % 2 + 1 # ((1P % 2 = 1) + 1 == 2P) and ((2P % 2 = 0) + 1 == 1P)
 
 def main():
     # Objects
     game = Game()
+    board = game.board
 
+    # Main "loop"
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # print(event.pos) => (66, 17)
+                pos = event.pos
+                col = pos[0] // SQSIZE # Converts from Cartesian co-ordinates to (columns, rows).
+                row = pos[1] // SQSIZE  
+
+                if board.empty_square(row, col):
+                    board.mark_sqr(row, col, game.player )
+                    game.next_turn()
 
         pygame.display.update()
 
